adbdfd27766e33025e459639751190a2bef33c51
unknown <charleshzx1@gmail.com>
2024-08-13 23:44:12 +0800

    Add new class: ModBlocks

--- src/main/java/com/xian/testmod/TestMod.java
+++ src/main/java/com/xian/testmod/TestMod.java
@@ -19,6 +19,7 @@ public class TestMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
 		/* Call to add mod items into the game */
 		ModItems.registerModItems();
 		ModItemGroups.registerModGroups();
---
+++ src/main/java/com/xian/testmod/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package com.xian.testmod.block;
+
+import com.xian.testmod.TestMod;
+
+public class ModBlocks {
+    public static void registerModBlocks(){
+        TestMod.LOGGER.info("Registering ModBlocks for " + TestMod.MOD_ID);
+    }
+}
